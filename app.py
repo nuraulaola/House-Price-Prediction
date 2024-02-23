b1be885b1a0027d7c02d209be05480161610466f
Nur Aula <148666180+nuraulaola@users.noreply.github.com>
2024-02-23 15:09:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,21 @@ import pandas as pd
 import numpy as np
 
 with open('Label_Encoders.pkl', 'rb') as file:
-        Label_Encoders = pickle.load(file)
+    label_encoders = pickle.load(file)
 
 with open('ridge_reg_model.pkl', 'rb') as file:
-        Ridge_Model = pickle.load(file)
+    Ridge_Model = pickle.load(file)
 
 with open('lasso_reg_model.pkl', 'rb') as file:
-        Lasso_Model = pickle.load(file)
+    Lasso_Model = pickle.load(file)
+
+# Maximal values for input columns
+maximal_values = {
+    "made": 2022,
+    "squareMeters": 5000.0,
+    "numberOfRooms": 6,
+    # Add more key-value pairs for other features
+}
 
 # Streamlit app
 def main():
@@ -31,7 +39,7 @@ def predict_ridge():
     st.header("Ridge Regression Prediction")
 
     # Get user input
-    user_input = get_user_input()
+    user_input = get_user_input(maximal_values)
 
     # Transform categorical variables using label encoders
     for col in user_input.select_dtypes(include=['object']).columns:
@@ -48,7 +56,7 @@ def predict_lasso():
     st.header("Lasso Regression Prediction")
 
     # Get user input
-    user_input = get_user_input()
+    user_input = get_user_input(maximal_values)
 
     # Transform categorical variables using label encoders
     for col in user_input.select_dtypes(include=['object']).columns:
@@ -61,14 +69,14 @@ def predict_lasso():
     # Display Lasso prediction
     st.write(f"Predicted Price (Lasso Regression): {int(prediction[0])}")
 
-def get_user_input():
+def get_user_input(maximal_values):
     # Create input form using Streamlit
     st.sidebar.header('User Input Parameters')
 
     # Add input elements for the user to enter data
-    made = st.sidebar.number_input("Enter Year Made:", min_value=1800, max_value=2022, value=2000)
-    square_meters = st.sidebar.number_input("Enter Square Meters:", min_value=0.0, value=100.0)
-    number_of_rooms = st.sidebar.number_input("Enter Number of Rooms:", min_value=0, value=3)
+    made = st.sidebar.number_input("Enter Year Made:", min_value=1800, max_value=maximal_values["made"], value=2000)
+    square_meters = st.sidebar.number_input("Enter Square Meters:", min_value=0.0, max_value=maximal_values["squareMeters"], value=100.0)
+    number_of_rooms = st.sidebar.number_input("Enter Number of Rooms:", min_value=0, max_value=maximal_values["numberOfRooms"], value=3)
     has_storage_room = st.sidebar.checkbox("Has Storage Room")
     has_guest_room = st.sidebar.checkbox("Has Guest Room")
     # Add more input elements for other features as needed
